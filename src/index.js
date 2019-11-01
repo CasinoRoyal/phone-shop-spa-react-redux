b0eac54902c7b63a733ff66515d141c05970edc1
CasinoRoyal <lux_ex_tenebris@outlook.com>
2019-11-01 13:16:23 +0200

    Init project

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux';
+
+import createRootReducer from 'redux/reducers/root.reducer';
+import Layout from 'containers/layout';
+
+import './main.css';
+
+const history = createBrowserHistory();
+const middlewares = [thunk, routerMiddleware(history)]
+
+const store = createStore(
+  createRootReducer(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Layout />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
